88697e71591d5d64d7c783349e4771976afb195b
Kyledude252 <kyleowena@gmail.com>
2024-01-31 13:09:15 -0800

    Initial Commit

---
+++ Project Forefront/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project Forefront
+//
+//  Created by Kyle Alexander on 2024-01-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project Forefront/Project_ForefrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_ForefrontApp.swift
+//  Project Forefront
+//
+//  Created by Kyle Alexander on 2024-01-31.
+//
+
+import SwiftUI
+
+@main
+struct Project_ForefrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

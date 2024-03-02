bb936402d917e37751410c8dc9844a910b61d128
Kyledude252 <kyleowena@gmail.com>
2024-03-01 22:42:17 -0800

    Added function for deleting cubes with explosion

--- Project Forefront/Level.swift
+++ Project Forefront/Level.swift
@@ -12,6 +12,7 @@ class Level: SCNNode {
     let squareSize: CGFloat = 0.25
     let numRows: Int
     let numCols: Int
+    var cubeGrid: [[SCNNode?]]?
     
     // COLORS
     let lightBrown = UIColor(red: 210/255, green: 180/255, blue: 140/255, alpha: 1.0)
@@ -24,9 +25,19 @@ class Level: SCNNode {
     init(levelWidth: CGFloat, levelHeight: CGFloat) {
         self.numRows = Int(levelWidth / squareSize)
         self.numCols = Int(levelHeight / squareSize)
+        self.cubeGrid = Array(repeating: Array(repeating: nil, count: numCols), count: numRows)
         super.init()
         
         setupLevel()
+        /*
+         //some hardcoded example explosions
+         deleteCubes(row: 100, col: 30, radius: 20)
+         deleteCubes(row: 150, col: 50, radius: 10)
+         deleteCubes(row: 120, col: 20, radius: 15)
+         deleteCubes(row: 200, col: 50, radius: 40)
+         deleteCubes(row: 250, col: 30, radius: 5)
+         */
+
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -51,12 +62,33 @@ class Level: SCNNode {
                                              materials: materials)
 
                 squareNode.position = SCNVector3(CGFloat(row) * squareSize, -squareSize / 2, CGFloat(col) * squareSize)
+                
+                cubeGrid![row][col] = squareNode
 
                 addChildNode(squareNode)
             }
         }
     }
     
+    func deleteCubes(row: Int, col: Int, radius: Int){
+        let rowMin = max(row - radius, 0)
+        let rowMax = min(row + radius, numRows - 1)
+        let colMin = max(col - radius, 0)
+        let colMax = min(col + radius, numCols - 1)
+        
+        for x in rowMin...rowMax{
+            for y in colMin...colMax{
+                
+                let circleRadius = sqrt(pow(Double(x - row), 2) + pow(Double(y - col), 2))
+                if(circleRadius < Double(radius)){
+                    cubeGrid![x][y]?.removeFromParentNode()
+                }
+
+            }
+        }
+
+        cubeGrid![row][col]?.removeFromParentNode()
+    }
     
     func delete() {
         self.removeFromParentNode()

a719e706a3d5be258656bc1b6d6c554101846655
Joseph Harris <jharris99@my.bcit.ca>
2024-04-17 16:05:02 -0700

    Documented SceneKitView.swift

--- Project Forefront/SceneKitView.swift
+++ Project Forefront/SceneKitView.swift
@@ -1,19 +1,24 @@
-//
-//  SceneKitView.swift
-//  Project Forefront
-//
-//  Created by user on 2/13/24.
-//
-
 import Foundation
 import SwiftUI
 import SceneKit
 
+
+/**
+ Struct containing a scene and a MainSceneViewModel.
+ 
+ SceneKitView inherits from UIViewControllerRepresentable.
+ */
 struct SceneKitView: UIViewControllerRepresentable {
     let scene: SCNScene
     let mainSceneViewModel: MainSceneViewModel
 
     
+    /**
+     Function for making a UIViewController.
+     
+     - parameter context: the Context of the app.
+     - returns: a GameViewController.
+     */
     func makeUIViewController(context: Context) -> GameViewController {
         let vc = GameViewController(scene: scene)
         if (scene == mainSceneViewModel.scene) {
@@ -29,42 +34,41 @@ struct SceneKitView: UIViewControllerRepresentable {
             context.coordinator.createPause(on: vc.view as! SCNView)
         }
         return vc
-
     }
     
+    
+    /**
+     Function for updating the UIViewController.
+     
+     - parameter uiViewController: a GameViewController for the app.
+     - parameter context: the Context object for the UI/game.
+     
+     This function is necessary but unused.
+     */
     func updateUIViewController(_ uiViewController: GameViewController, context: Context) {
         // No updates needed
     }
     
     typealias UIViewControllerType = GameViewController
    
-//    func makeUIView(context: Context) -> SCNView {
-//        let scnView = SCNView(frame: .zero)
-//        //scnView.scene = scene
-//        scnView.scene = scene
-//        
-//        let main = mainSceneViewModel.scene
-//         main.scnView = scnView
-//        //firing code ---
-//        //button position
-//        context.coordinator.setupFireButton(on: scnView)
-//        context.coordinator.setupGestureRecognizers(on: scnView)
-//        
-//        return scnView
-//    }
-//    
-//    func updateUIView(_ scnView: SCNView, context: Context) {
-//        // No updates needed
-//    }
-//    
     
-    //Makes coordinator, sends an instance
+    /**
+     Function that makes an instance of a Coordinator for the scene.
+     
+     - returns: the Coordinator of the scene.
+     */
     func makeCoordinator() -> Coordinator {
         Coordinator(self, mainScene: mainSceneViewModel.scene)
     }
-    //Coordinator class, needed to handle functions from MainScene
-    //without coordinator functins from main scene would apply to another instance of main scene
-    // which is not veiwable with the current view
+    
+    
+    /**
+     Class representing a Coordinator.
+     
+     Coordinator inherits from NSObject, UIPickerViewDelegate, UIPickerViewDataSource.
+     
+     The coordinator is needed to handle certain functions in the main scene. Without it, the functions may apply to the wrong view.
+     */
     class Coordinator: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
         
         // Scenekit View
@@ -81,8 +85,6 @@ struct SceneKitView: UIViewControllerRepresentable {
         var closeButton: UIButton?
         // button to induce pause
         var pauseButton: UIButton?
-        //toggle boolean for fire mode on/off
-//        var fireModeOn = false;
         // used for self reference to figure out who's turn it is
         var playerTurn: Int = 1
         // shot type used to alter launch projectile
@@ -95,12 +97,27 @@ struct SceneKitView: UIViewControllerRepresentable {
             self.mainScene = mainScene
         }
         
-        //sets up gesture
+        
+        /**
+         Function for setting up the pan gesture.
+         
+         - parameter view: the view in which to create the gesture.
+         
+         Called by:
+         
+         SceneKitView.makeUIViewController()
+         */
         func setupGestureRecognizers(on view: SCNView) {
             let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
             view.addGestureRecognizer(panGesture)
         }
-        //handles pan, gets location on screen (2D) and translate it to 3D
+        
+        
+        /**
+         Function for handling the pan gesture for aiming and firing the weapons.
+         
+         - parameter gesture: a UIPanGestureRecognizer that returns the actual gesture data.
+         */
         @objc func handlePan(gesture: UIPanGestureRecognizer) {
             //set scene from MainScene
             //Does not allow gesture to handle if fire mode is not toggled
@@ -123,6 +140,16 @@ struct SceneKitView: UIViewControllerRepresentable {
             }
         }
         
+        
+        /**
+         Function for setting up a picker view for the weapon select.
+         
+         - parameter view: the view in which to create the UIPickerView
+         
+         Called by:
+         
+         SceneKitView.makeUIViewController()
+         */
         func setupPicker(on view: SCNView) {
             let pickerView = UIPickerView(frame: CGRect(x: 20, y: 75, width: 100, height: 60))
             pickerView.delegate = self
@@ -132,26 +159,67 @@ struct SceneKitView: UIViewControllerRepresentable {
             pickerView.layer.cornerRadius = 10
             view.addSubview(pickerView)
             weapons = pickerView
-            
         }
         
+        
+        /**
+         Function for returning the number of components in a PickerView.
+         
+         - parameter pickerView: pickerview to ask for number of components in.
+         - returns: 1, as this is a static PickerView with 1 component.
+         */
         func numberOfComponents(in pickerView: UIPickerView) -> Int {
             return 1
         }
         
+        
+        /**
+         Asks the data source for the number of rows for a specified component.
+         
+         - parameter pickerView: UIPickerView to ask.
+         - parameter component: component to ask the number of rows for.
+         - returns: 3, as this is a static UIPickerView with 1 component and 3 rows.
+         */
         func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
             return 3
         }
         
+        
+        /**
+         Function that returns the String in the pickerview for a specific component and specific row.
+         
+         - parameter pickerView: UIPickerView to ask.
+         - parameter row: Int representing a row in the component.
+         - parameter component: Int representing a component in the UIPickerView
+         - returns: the string in the requested row of the asked component.
+         */
         func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
             return shotTypes[row]
         }
         
+        
+        /**
+         Function that prints the selected row of a component's item, formatted to display weapon type.
+         
+         - parameter pickerView: UIPickerView to ask.
+         - parameter row: Int representing a row in the component.
+         - parameter component: Int representing a component in the UIPickerView
+         */
         func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
             shotType = row + 1
             print("shot type: ", shotType)
         }
         
+        
+        /**
+         Function for setting up the pause button.
+         
+         - parameter view: the view in which to create the pause menu.
+         
+         Called by:
+         
+         SceneKitView.makeUIViewController()
+         */
         func setupPause(on view: SCNView) {
             let button = UIButton(frame: CGRect(x: 20, y: 20, width: 100, height: 50))
             //button color, tittle, action
@@ -163,39 +231,10 @@ struct SceneKitView: UIViewControllerRepresentable {
             pauseButton = button
         }
         
-//        // Creates fire button on screen view, so it remains on screen when panning
-//        func setupFireButton(on view: SCNView) {
-//            let button = UIButton(frame: CGRect(x: 20, y: 20, width: 100, height: 50))
-//            //button color, tittle, action
-//            button.backgroundColor = UIColor(red: 0.35, green: 0.35, blue: 0.65, alpha: 1.0)
-//            button.layer.cornerRadius = 10
-//            button.setTitle("Move Mode", for: .normal)
-//            button.addTarget(self, action: #selector(toggleFire), for:.touchUpInside)
-//            view.addSubview(button)
-//            self.toggleButton = button
-//        }
-        
-        //swap text of UIButton,
-//        func fireModeText() {
-//            if let button = toggleButton {
-//                if button.title(for: .normal) == "Fire Mode" {
-//                    button.setTitle("Move Mode", for: .normal)
-//                    button.backgroundColor = UIColor(red: 0.35, green: 0.35, blue: 0.65, alpha: 1.0)
-//                    //toggles fire mode off
-//                    fireModeOn = false
-//                    //removes line drawn, another safeguard to remove this artifact
-//                    mainScene.removeLine()
-//                    //toggle tank movement back on
-//                    mainScene.toggleTankMove(move: true)
-//                } else if button.title(for: .normal) == "Move Mode" {
-//                    button.setTitle("Fire Mode", for: .normal)
-//                    button.backgroundColor = UIColor(red: 0.65, green: 0.20, blue: 0.20, alpha: 1.0)
-//                    fireModeOn = true //toggles fire mode on
-//                    mainScene.toggleTankMove(move: false)
-//                }
-//            }
-//        }
-        // called when pause is pressed
+
+        /**
+         Function for handling pausing the game.
+         */
         @objc func pauseGame() {
             print("Pause woohooo")
             mainScene.pauseTriggered()
@@ -204,7 +243,17 @@ struct SceneKitView: UIViewControllerRepresentable {
             closeButton?.isHidden = false
             mainScene.isPaused = true
         }
-        // buttons for pause menu
+        
+        
+        /**
+         Function for creating the pause menu.
+         
+         - parameter view: the view in which to create the pause menu.
+         
+         Called by:
+         
+         SceneKitView.makeUIViewController()
+         */
         @objc func createPause(on view: SCNView) {
             // Return to main
             let button1 = UIButton(frame: CGRect(x: 350, y: 100, width: 150, height: 50))
@@ -226,6 +275,14 @@ struct SceneKitView: UIViewControllerRepresentable {
             closeButton?.isHidden = true
         }
         
+        
+        /**
+         Function to handle unpausing of the game.
+         
+         Called by:
+         
+         SceneKitView.Coordinator.returnToMain()
+         */
         @objc func unPause() {
             returnButton?.isHidden = true
             closeButton?.isHidden = true
@@ -234,25 +291,38 @@ struct SceneKitView: UIViewControllerRepresentable {
             mainScene.isPaused = false
         }
         
-        //Grabs function from main scene
-//        @objc func toggleFire() {
-//            fireModeText()
-//            //does not trigger toggle fire unless fireMode is on
-//            mainScene.toggleFire(isFireMode: fireModeOn)
-//        }
-        // doesn't need to be used
+        
+        /**
+         Function to handle disabling control of the buttons.
+         
+         Called by:
+         
+         SceneKitView.Coordinator.pauseGame()
+         */
         @objc func disableButtons() {
             toggleButton?.isEnabled = false
             weapons?.isUserInteractionEnabled = false
             pauseButton?.isEnabled = false
         }
         
+        
+        /**
+         Function to handle enabling control of the buttons.
+         
+         Called by:
+         
+         SceneKitView.Coordinator.upPause()
+         */
         @objc func enableButtons() {
             toggleButton?.isEnabled = true
             weapons?.isUserInteractionEnabled = true
             pauseButton?.isEnabled = true
         }
         
+        
+        /**
+         Function to handle returning to the main menu from the pause screen.
+         */
         @objc func returnToMain() {
             unPause()
             mainScene.resetToStartScreen()
